e1943d6ad7aadf672985739982648c30093372c5
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-10-10 09:52:55 +0300

    pcking up

--- schools/urls.py
+++ schools/urls.py
@@ -487,4 +487,11 @@ urlpatterns = [
     path('create_fees_type/', views.create_fees_type, name='create_fees_type'),
     path('ajax/load_fee_amount/', views.load_fee_amount, name='ajax_load_fee_amount'),
     path('create_invoice/', views.create_invoice, name='create_invoice'),
+
+    path('ajax/load-exams/', views.load_exams, name='ajax_load_exams'),
+    path('ajax/load-classrooms/', views.load_classrooms, name='ajax_load_classrooms'),
+    path('ajax/load-sections/', views.load_sections, name='ajax_load_sections'),
+    path('ajax/load-roles/', views.load_roles, name='ajax_load_roles'),
+
+
 ]
--- schools/views.py
+++ schools/views.py
@@ -6157,7 +6157,7 @@ def fee_type_delete(request, fee_type_pk):
                                              )
     return JsonResponse(data)
 
-def create_fees_type(request):
+    def create_fees_type(request):
       if request.method=="POST":
           form=FeeTypeForm(request.POST,request.FILES)
           if form.is_valid():
@@ -6197,3 +6197,25 @@ def create_invoice (request):
         'form': form,
     }
     return render(request, 'invoices/create_invoice.html', context)
+#keeping track of the school id on the invoice being created
+def load_fee_types(request):
+    school_id = request.GET.get('school')
+    fee_types = FeeType.objects.filter(school_id=school_id).order_by('fee_type')
+    return render(request, 'filter/fee_dropdown_list_options.html', {'fee_types': fee_types})
+
+#load fee amount depending on the selection in fee types field
+def load_ft_amount(request):
+    classroom_id = request.GET.get('classroom')
+    fee_amounts = FeeType.objects.filter(Class_id=classroom_id).order_by('Class_Amount')
+    return render(request, 'filter/fee_amount_dropdown_list_options.html', {'fee_amounts': fee_amounts})
+
+def load_ft_classrooms(request):
+    classroom_id = request.GET.get('classroom')
+    sections = Section.objects.filter(classroom_id=classroom_id).order_by('section')
+    return render(request, 'filter/section_dropdown_list_options.html', {'sections': sections})
+
+
+def load_ft_students(request):
+    classroom_id = request.GET.get('classroom')
+    students = Student.objects.filter(classroom_id=classroom_id).order_by('user')
+    return render(request, 'filter/student_dropdown_list_options.html', {'students': students})
