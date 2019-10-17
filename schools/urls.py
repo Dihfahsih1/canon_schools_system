c3dd83a5f53109692a6c73f48153bcf410a2f35b
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-10-17 11:49:28 +0300

    adding urls

--- schools/urls.py
+++ schools/urls.py
@@ -484,6 +484,9 @@ urlpatterns = [
     path('ajax/load-students/', views.load_students, name='ajax_load_students'),
     path('ajax/load-fee_types/', views.load_fee_types, name='ajax_load_fee_types'),
     path('ajax/load_fee_amount/', views.load_fee_amount, name='ajax_load_fee_amount'),
+    path('ajax/load-roles/', views.load_roles, name='ajax_load_roles'),
+    path('ajax/load-teachers/', views.load_teachers, name='ajax_load_teachers'),
+    path('ajax/load_employees/', views.load_employees, name='ajax_load_employees'),
     #path('ajax/load-total-allowances/', views.load_total_allowances, name='ajax_load_total_allowances'),
 #
     path('create_fees_type/', views.create_fees_type, name='create_fees_type'),
