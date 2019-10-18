ebbdaed3889930d8dfa9ebc5bdaf01d4eea181fc
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-10-18 15:48:32 +0300

    using the function based views instead of CBV to implement updating

--- schools/views.py
+++ schools/views.py
@@ -6229,3 +6229,14 @@ def load_employee_salary_grade(request):
     school_id = request.GET.get('school')
     salarygrades = SalaryGrade.objects.filter(school_id=school_id).order_by('grade_name')
     return render(request, 'filter/salarygrade_dropdown_list_options.html', {'salarygrades': salarygrades})
+
+def Pay_Employee(request, pk):
+    item = get_object_or_404(SalaryGrade, id=pk)
+    if request.method == "POST":
+        form =  SalaryGradeForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('SalaryPayment')
+    else:
+        form =  SalaryGradeForm(instance=item)
+        return render(request, 'payroll/pay_employee.html', {'form': form})
