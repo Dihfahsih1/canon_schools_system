66d981001638d828f33aa555715de36265d4061b
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-11-07 10:58:50 +0300

    seriously

--- schools/forms.py
+++ schools/forms.py
@@ -937,7 +937,7 @@ class MonthlySalaryPaidForm(forms.ModelForm):
         fields = ('academic_year','employees_id','school', 'employee', 'grade_name', 'basic_salary', 'house_rent', 'transport_allowance', 'medical_allowance','over_time_hourly_pay', 'provident_fund', 'hourly_rate', 'total_allowance','total_deduction','gross_salary','net_salary','over_time_total_hour','over_time_amount','Bonus','Penalty','Month','Payment_Method','Expenditure_Head','note','Cheque_Number','Bank_Name')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['academic_year'].queryset = Years.objects.none()
+        self.fields['academic_year'].queryset = Year.objects.none()
         if 'school' in self.data:
             try:
                 school_id = int(self.data.get('school'))
